852b414055e4fff1c613183d81f6096862b67406
Mostafa <mostafa.ashrf20@gmail.com>
2023-08-23 04:37:33 +0300

    test for Task1

--- _printf.c
+++ _printf.c
@@ -10,10 +10,16 @@ int _printf(const char *format, ...)
 {
 	int printCount = 0;
 
+    unsigned int i = 0;
+
 	va_list args;
 
 	va_start(args, format);
 
+    if (!format || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
 	while (*format != '\0')
 	{
 		if (*format == '%')
